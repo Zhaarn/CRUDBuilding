7e2e2b2834370a6d7caf5d74cb3d75151db1be48
am_dev <amdirtylabdeveloper@gmail.com>
2018-02-08 11:04:16 -0500

    Added some stuff

--- MnA3.0/MnA/MnA/urls.py
+++ MnA3.0/MnA/MnA/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import crudbuilder
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crud/', crudbuilder.urls)
 ]
